f229098cec6038bfab85d6ed49b0b05ba6968f81
Rafel Bover Lladó <rlladobover@gmail.com>
2021-10-04 13:32:06 +0200

    ejercicio2
    cambios en los scripts, myFirstScript y vehicle

---
+++ Programacion/Assets/Script/MyFirstScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    public int playerAge = 35;
+    private float playerSpeed = 5.25f;
+    public string plyerName = "Rafa";
+    [SerializeField] private bool gameOver = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Programacion/Assets/Script/Vehicle.cs
+++ Programacion/Assets/Script/Vehicle.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
+    //sonido que haran los vehículos 
+    [SerializeField] private string sound = "¡Brum, brum!";
+
+
     // Start is called before the first frame update
     void Start()
     {
