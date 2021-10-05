4740d67ac5520e5085c47f4ca4c4bee4e28c9bef
Rafel Bover Lladó <rlladobover@gmail.com>
2021-10-05 11:09:45 +0200

    programacion
    condicionales y operaciones númericas

--- Programacion/Assets/Script/MyFirstScript.cs
+++ Programacion/Assets/Script/MyFirstScript.cs
@@ -4,21 +4,64 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+    /*
     public int playerAge = 35;
     private float playerSpeed = 5.25f;
-    public string plyerName = "Rafa";
+    
     [SerializeField] private bool gameOver = false;
+    */
 
+    public string playerName = "Rafa";
+    public string enemyName = "Rafa ";
+
+    [SerializeField] private int x = 5;
+    [SerializeField] private int y = 3;
+    [SerializeField] private int z = 3;
+
+    //Esta bolean es para hacer que llueva
+    [SerializeField] private bool isRaining;
 
     // Start is called before the first frame update
     void Start()
     {
+       
+     /*
+     Debug.Log(playerAge);
+     Debug.Log(message: $"Hola {playerName}!");
+     Debug.Log(message: $"Suma: {x} + {y} ={x + y}");
+     Debug.Log(message: "Resta: " + x + " - " + y + " = " + (x - y));
+     Debug.Log(message: string.Format("Producto: {0} * {1} = {2}", x, y, x * y));
+     Debug.Log(message: string.Format("Division: {0} / {1} = {2}", x, y, x / y));
+     */  
+     
+     if (playerName == enemyName)
+     {
+
+      Debug.Log(message: "Player y enemigo se llaman igual");
+
+     }
+
+     if(playerName != enemyName)
+     {
+
+      Debug.Log(message: "Player y enemigo tienen distinto nombre");
+
+     }
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+   
+     //Mensaje que saldra cuando se active la lluvia
+     /*
+     if (isRaining)
+     {
+     Debug.Log(message: "Llévate un paraguas");
+     }
+     */
+
     }
 }
--- Programacion/Assets/Script/Vehicle.cs
+++ Programacion/Assets/Script/Vehicle.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
-    //sonido que haran los vehículos 
+    // Sonido que harán los vehículos 
     [SerializeField] private string sound = "¡Brum, brum!";
+    
+    // Nombres de los vehículos
+    [SerializeField] private string name = "Coche";
+    
+    // Número de ruedas que tienen los vehículos
+    [SerializeField] private int numberWheels = 4;
 
+    // Sirena de la ambulancia
+    [SerializeField] private bool hasSiren = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+     
+        // Debug.Log(message: $"{name} tiene {numberWheels} ruedas y hace {sound}");
+        Debug.Log(message: string.Format("{1} tiene {2} ruedas y hace {0}", sound, name, numberWheels)); 
+       
+        // Si la sirena se activa aparecerá una notificación
+        if(hasSiren == true)
+        {
+            Debug.Log(message: $"{name} tiene sirena");
+        }
+    
     }
 
     // Update is called once per frame
